6199736886f339178f9a3d35e7826a104d2fa84a
Sunil Paidimarri <hisunil@codeaurora.org>
2019-03-26 15:35:34 -0700

    data-kernel: EMAC: Handle dynamic MTU config
    Fix the change mtu function.
    Max MTU supported in IPA offload path is upto 2KB.
    
    Change-Id: I6ad85d58d9bd0c7b0c5e9971fcab25ea0976a02a
    Acked-by: Rahul Kawadgave <rahulak@qti.qualcomm.com>
    Signed-off-by: Sunil Paidimarri <hisunil@codeaurora.org>

--- drivers/emac-dwc-eqos/DWC_ETH_QOS_yheader.h
+++ drivers/emac-dwc-eqos/DWC_ETH_QOS_yheader.h
@@ -382,10 +382,10 @@ extern void *ipc_emac_log_ctxt;
 #define VLAN_HLEN 0
 #endif
 
-#define PADDING_ISSUE (2*8)
-#define DWC_ETH_QOS_ETH_FRAME_LEN (ETH_FRAME_LEN + ETH_FCS_LEN + VLAN_HLEN + PADDING_ISSUE)
+#define DWC_ETH_QOS_ETH_FRAME_PADDING_ISSUE (2*8)
+#define DWC_ETH_QOS_ETH_FRAME_LEN (ETH_FRAME_LEN + ETH_FCS_LEN + VLAN_HLEN)
 
-#define DWC_ETH_QOS_ETH_FRAME_LEN_IPA	((1<<11) + PADDING_ISSUE) /*IPA can support 2KB max pkt length*/
+#define DWC_ETH_QOS_ETH_FRAME_LEN_IPA	((1<<11)) /*IPA can support 2KB max pkt length*/
 
 #define FIFO_SIZE_B(x) (x)
 #define FIFO_SIZE_KB(x) (x * 1024)
@@ -394,7 +394,8 @@ extern void *ipc_emac_log_ctxt;
 #define DWC_ETH_QOS_MAX_DATA_PER_TX_BUF BIT(12)	/* for testing purpose: 4 KB Maximum data per buffer pointer(in Bytes) */
 #define DWC_ETH_QOS_MAX_DATA_PER_TXD (DWC_ETH_QOS_MAX_DATA_PER_TX_BUF * 2)	/* Maxmimum data per descriptor(in Bytes) */
 
-#define DWC_ETH_QOS_MAX_SUPPORTED_MTU 16380
+#define DWC_ETH_QOS_MAX_MTU_SIZE (1 << 11)     /*2KB to support gaint packets*/
+#define DWC_ETH_QOS_MAX_SUPPORTED_MTU DWC_ETH_QOS_MAX_MTU_SIZE
 #define DWC_ETH_QOS_MAX_GPSL 9000 /* Default maximum Gaint Packet Size Limit */
 #define DWC_ETH_QOS_MIN_SUPPORTED_MTU (ETH_ZLEN + ETH_FCS_LEN + VLAN_HLEN)
 
@@ -1890,6 +1891,7 @@ struct DWC_ETH_QOS_prv_data {
 	struct cdev* avb_class_b_cdev;
 	struct class* avb_class_b_class;
 
+	bool jumbo_frame_supported;
 };
 
 struct ip_params {
